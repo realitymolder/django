80bcb42f2fa0501751c7210986df4b0db19eff79
Daniel Toubul <molder@toubul.eu>
2022-09-19 00:57:51 +0300

    :bug: logo path fixed + small fix for routes

--- src/lib/shared/atoms/LogoAtom.tsx
+++ src/lib/shared/atoms/LogoAtom.tsx
@@ -5,7 +5,7 @@ const LogoAtom: Component = () => {
   return (
     <NavLink href={"/"}>
       {/* <h1 class="text-4xl font-bold">DJANGO</h1>*/}
-      <img src="../../assets/images/django-logo.png" alt="DJANGO logo" />
+      <img src="../../../assets/images/django-logo.png" alt="DJANGO logo" />
     </NavLink>
   );
 };
